a7c1fb6ecc73e834e0ca8d3e65203b79eb12fc23
RVCDv <101155145+RVCDv@users.noreply.github.com>
2022-10-07 21:22:11 +0800

    w

---
+++ lib/import.lua
@@ -0,0 +1,21 @@
+local IMPORT_CACHE = {}
+
+local function import(asset)
+    if IMPORT_CACHE[asset] then
+        return IMPORT_CACHE[asset]
+    end
+
+    if asset:find("rbxassetid://") then
+        local e = game:GetObjects(asset)[1]
+        IMPORT_CACHE[asset] = e
+        return e
+    else
+        local src = loadstring(
+            game:HttpGet('https://raw.githubusercontent.com/RVCDv/eSyn/main/build/src/' .. asset .. '.lua')
+        )()
+        IMPORT_CACHE[asset] = src
+        return src
+    end
+end
+
+return import
