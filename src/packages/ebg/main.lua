d3a80a15d557fbbb136763c14a9764371041e6c9
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2022-10-22 19:28:32 +0800

    Update main.lua

--- src/packages/ebg/main.lua
+++ src/packages/ebg/main.lua
@@ -555,7 +555,7 @@ return {
                         if foundForceFied then cleanPoints() return end
                         local hrp = targetChar:FindFirstChild("HumanoidRootPart")
                         local flipsHolder = targetChar:FindFirstChild("FlipsHolder")
-                        print(flipsHolder ~= nil)
+                        print(flipsHolder ~= nil, flipsHolder)
                         if hrp then
                             if autoPredictIndex then
                                 falsePredictionIndex = math.clamp(math.floor((hrp.Position - rhrp.Position).Magnitude / 10 + 0.5), 1, NUMS_OF_PREDICTIONS)
