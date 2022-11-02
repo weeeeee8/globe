a92f392981e982a853702a9dbb1a603cc814486c
RVCDv <101155145+weeeeee8@users.noreply.github.com>
2022-11-02 18:00:28 +0800

    Update main.lua

--- src/packages/funkyfriday/main.lua
+++ src/packages/funkyfriday/main.lua
@@ -2,6 +2,8 @@ local RunService = game:GetService("RunService")
 local env = assert(getgenv, "[GLOBE] getgenv cannot be found, executor might not be supported")()
 local Maid = import('lib/maid')
 
+local RNG = Random.new()
+
 return {
     init = function(windw)
         local tab = windw:Tab{Text = "Funky Friday"}
@@ -29,45 +31,62 @@ return {
             }
 
             local noteAccuracy = 0.05
+            local tryNotAcccurate = false
             local section = tab:Section{Text = "AutoHit Note Options"}
+            
+            section:Input{
+                Text = "Set Accuracy",
+                Placeholder = "Accuracy <0-0.3>",
+                Callback = function(str)
+                    local num = tonumber(str)
+                    if num then
+                        num = math.clamp(num, 0, 0.3)
+                        noteAccuracy = num
+                    end
+                end
+            }
 
             section:Toggle{
-                Text = "Toggle Autohit",
+                Text = "Should Try not Accurate",
                 Callback = function(toggled)
-                    if toggled then
-                        local GUI = game.Players.LocalPlayer.PlayerGui:WaitForChild("GameUI")
-                        local Arrows = GUI:WaitForChild("Arrows")
-                        for _, frame in ipairs(Arrows.Left.Arrows:GetChildren()) do
-                            if not frame:IsA("Frame") then continue end
-                            local strIndex = tostring(frame.Name:sub(6, #frame.Name))
-                            tempMaid:GiveTask(frame.InnerFrame.Column.ChildAdded:Connect(function(c)
-                                local name = c.Name:lower()
-                                if name == "frame" then
-                                    push_stack(c)
-                                elseif name == "note" then
-                                    local data = {
-                                        note = c,
-                                        keycode = KEYCODE_BY_INDEX[strIndex],
-                                        index = strIndex,
-                                    }
-            
-                                    local foundFrame = pop_stack()
-                                    if foundFrame then
-                                        data.frameSizeTime = (foundFrame.Size.Y.Scale * 0.35) - noteAccuracy
-                                    end
-                                    RUNNING_NOTES[data] = true
-            
-                                    c.Destroying:Once(function()
-                                        if not data.frameSizeTime then
-                                            keyrelease(data.keycode)
-                                        end
-                                        RUNNING_NOTES[data] = nil
-                                    end)
+                    tryNotAcccurate = toggled
+                end
+            }
+
+            section:Button{
+                Text = "Toggle Autohit",
+                Callback = function()
+                    local GUI = game.Players.LocalPlayer.PlayerGui:WaitForChild("GameUI")
+                    local Arrows = GUI:WaitForChild("Arrows")
+                    tempMaid:DoCleaning()
+                    for _, frame in ipairs(Arrows.Left.Arrows:GetChildren()) do
+                        if not frame:IsA("Frame") then continue end
+                        local strIndex = tostring(frame.Name:sub(6, #frame.Name))
+                        tempMaid:GiveTask(frame.InnerFrame.Column.ChildAdded:Connect(function(c)
+                            local name = c.Name:lower()
+                            if name == "frame" then
+                                push_stack(c)
+                            elseif name == "note" then
+                                local data = {
+                                    note = c,
+                                    keycode = KEYCODE_BY_INDEX[strIndex],
+                                    index = strIndex,
+                                }
+        
+                                local foundFrame = pop_stack()
+                                if foundFrame then
+                                    data.frameSizeTime = (foundFrame.Size.Y.Scale * 0.35) - noteAccuracy
                                 end
-                            end))
-                        end
-                    else
-                        tempMaid:DoCleaning()
+                                RUNNING_NOTES[data] = true
+        
+                                c.Destroying:Once(function()
+                                    if not data.frameSizeTime then
+                                        keyrelease(data.keycode)
+                                    end
+                                    RUNNING_NOTES[data] = nil
+                                end)
+                            end
+                        end))
                     end
                 end
             }
@@ -75,7 +94,11 @@ return {
             oh.Maid:GiveTask(RunService.RenderStepped:Connect(function(dt)
                 for data in pairs(RUNNING_NOTES) do
                     local pos = data.note.Position.Y.Scale
-                    if pos < (0 + noteAccuracy) then
+                    local accuracy = noteAccuracy
+                    if tryNotAcccurate then
+                        accuracy = RNG:NextNumber(noteAccuracy, noteAccuracy+0.1)
+                    end
+                    if pos < (0 + accuracy) then
                         coroutine.wrap(function()
                             keypress(data.keycode)
                             if data.frameSizeTime then
